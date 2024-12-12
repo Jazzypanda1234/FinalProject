f7e4fea3c47508b73e0cd9aa27099931798387ab
Mathew Olsen <mathew.olsen@snow.edu>
2024-12-12 13:26:03 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.Program;
+
+ICreature[]? playerTeam = [null, null, null, null, null];
+ICreature[]? tempPlayerTeam = playerTeam;
+ICreature[]? enemyTeam;
+GameEngine? gameEngine = new GameEngine();
+Player? player = new Player(10, playerTeam);
+Shop? shop = new Shop(gameEngine);
+
+
+
+
+for (int i = 0; i < 4; i++)
+{
+    shop.ShopEngine(player);
+    enemyTeam = [null, null, null, null, null];
+    enemyTeam = gameEngine?.CreateEnemyTeam(enemyTeam, i);
+    gameEngine?.Combat(player, enemyTeam);
+}
+shop.ShopEngine(player);
+enemyTeam = [null, null, null, null, null];
+enemyTeam = gameEngine?.CreateEnemyTeam(enemyTeam, 2);
+enemyTeam[4] = gameEngine.randomBoss();
+gameEngine?.Combat(player, enemyTeam);
+Console.WriteLine("Game Over");
